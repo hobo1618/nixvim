7c3cefe992964df208593aaa79ecaefc53e0b420
hobo1618 <hobdenw@gmail.com>
2024-09-12 16:45:54 +0200

    add mdx.nvim treesitter extension

---
+++ config/plug/lsp/mdx.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "mdx.nvim";
+      #      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "davidmh";
+        repo = "mdx.nvim";
+        rev = "61b93f6576cb5229020723c7a81f5a01d2667d05";
+        hash = "sha257-CYcL+s1634UgquwUYp70iAD2xC6r87j6w5jYv90mUAg=";
+      };
+    })
+  ];
+}

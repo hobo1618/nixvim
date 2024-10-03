727b07f62a452003e327e363374b71cad8e9a4a1
hobo1618 <hobdenw@gmail.com>
2024-10-03 22:47:05 +0200

    update note_id_func for obsidian

--- config/plug/utils/obsidian.nix
+++ config/plug/utils/obsidian.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, helpers, ... }:
 {
   plugins.obsidian = {
     enable = true;
@@ -14,6 +14,24 @@
           path = "~/Documents/askerra/question-notes";
         }
       ];
+      note_id_func = ''
+        function(title)
+          -- see https://nix-community.github.io/nixvim/plugins/obsidian/settings/index.html
+
+          local suffix = ""
+          if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            title = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            return title
+          else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+          end
+          return tostring(os.time()) .. "-" .. suffix
+        end
+      '';
       mappings = {
         gf = {
           action = "require('obsidian').util.gf_passthrough";

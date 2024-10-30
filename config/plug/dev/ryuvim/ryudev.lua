a6d6a945b61bb42189fbf10a109eb6fb23790ae8
hobo1618 <hobdenw@gmail.com>
2024-10-30 21:53:33 +0100

    create minimal boilerplate

--- config/plug/dev/ryuvim/ryudev.lua
+++ config/plug/dev/ryuvim/ryudev.lua
@@ -1,3 +1,20 @@
-vim.api.nvim_create_user_command("RyuDev", function()
-	print("Hello from dev module in local dev directory!")
+local M = {}
+
+function M.setup()
+	-- Initialization code here
+	print("Plugin loadeds!")
+end
+
+function M.hello()
+	print("Hello from my pluginsss!")
+end
+
+vim.api.nvim_create_user_command("ReloadMyPlugin", function()
+	package.loaded["ryudev"] = nil
+	require("ryudev").setup()
 end, {})
+
+-- Define a command that users can call
+vim.api.nvim_create_user_command("RyuHello", M.hello, {})
+
+return M
--- config/plug/dev/ryuvim/ryuvim.nix
+++ config/plug/dev/ryuvim/ryuvim.nix
@@ -7,21 +7,21 @@
       };
     };
 
-  extraPlugins = with pkgs.vimUtils; [
-    (buildVimPlugin {
-      pname = "ryuvim";
-      version = "1.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "hobo1618";
-        repo = "ryuvim";
-        rev = "5e5d0599eee16741c1ae24a2090df4f87fb7640a";
-        hash = "sha256-K1dr3qLnuSK8AhE1jgcITgukeVY7jrwjsX9Xttahoks=";
-      };
-    })
-  ];
+  # extraPlugins = with pkgs.vimUtils; [
+  #   (buildVimPlugin {
+  #     pname = "ryuvim";
+  #     version = "1.0";
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "hobo1618";
+  #       repo = "ryuvim";
+  #       rev = "5e5d0599eee16741c1ae24a2090df4f87fb7640a";
+  #       hash = "sha256-K1dr3qLnuSK8AhE1jgcITgukeVY7jrwjsX9Xttahoks=";
+  #     };
+  #   })
+  # ];
 
   extraConfigLua = ''
-    require('ryuvim').setup()
+    -- require('ryuvim').setup()
     require('ryudev')
   '';
 

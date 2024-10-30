278fa8ec3cd30e081ba5bed811c9c32796c82813
hobo1618 <hobdenw@gmail.com>
2024-10-30 22:28:42 +0100

    add submodules

--- config/plug/dev/ryuvim_dev/ryuvim/init.lua
+++ config/plug/dev/ryuvim_dev/ryuvim/init.lua
@@ -1,9 +1,14 @@
 local M = {}
 
 -- Load modules
+M.list = require("ryuvim.commands.list")
 -- M.query = require("config.plug.dev.ryuvim_dev.ryuvim.commands.query")
 -- M.delete = require("config.plug.dev.ryuvim_dev.ryuvim.commands.delete")
 -- Load other modules similarly...
+
+function M.hello()
+	print("Hello from my plugin!")
+end
 
 vim.api.nvim_create_user_command("RyuHello", M.hello, {})
 
@@ -12,6 +17,7 @@ function M.setup()
 	print("Plugin loaded!")
 	-- M.query.setup()
 	-- M.delete.setup()
+	M.list.setup()
 	-- Setup calls for other commands
 end
 

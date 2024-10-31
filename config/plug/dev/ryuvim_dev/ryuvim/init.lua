1156ebd9e97b4270e08fd2a10874aee424fe2f58
hobo1618 <hobdenw@gmail.com>
2024-10-31 20:31:47 +0100

    add funs

--- config/plug/dev/ryuvim_dev/ryuvim/init.lua
+++ config/plug/dev/ryuvim_dev/ryuvim/init.lua
@@ -1,23 +1,26 @@
 local M = {}
 
 -- Load modules
-M.list = require("ryuvim.commands.list")
+local list = require("ryuvim.commands.list")
+-- M.list = require("ryuvim.commands.list")
 -- M.query = require("config.plug.dev.ryuvim_dev.ryuvim.commands.query")
 -- M.delete = require("config.plug.dev.ryuvim_dev.ryuvim.commands.delete")
 -- Load other modules similarly...
 
 function M.hello()
 	print("Hello from my plugin!")
+	print(list.setup())
 end
 
 vim.api.nvim_create_user_command("RyuHello", M.hello, {})
+vim.api.nvim_create_user_command("FalkorList", list.run(), {})
 
--- Set up all commands
 function M.setup()
 	print("Plugin loaded!")
 	-- M.query.setup()
 	-- M.delete.setup()
-	M.list.setup()
+	-- M.list.setup()
+	-- list.setup()
 	-- Setup calls for other commands
 end
 

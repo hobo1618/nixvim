f2283876f6129e8e1ab1a85967a78263cd91d43b
hobo1618 <hobdenw@gmail.com>
2024-12-03 22:08:40 +0100

    build dadbob-x from github

--- config/plug/completion/vim-dadbod-completion.nix
+++ config/plug/completion/vim-dadbod-completion.nix
@@ -1,5 +1,15 @@
+{ pkgs, config, ... }:
 {
-  plugins.vim-dadbod-completion = {
-    enable = true;
-  };
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "vim-dadbod-completion";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "kristijanhusak";
+        repo = "vim-dadbod-ui";
+        rev = "04485bfb53a629423233a4178d71cd4f8abf7406";
+        hash = "sha256-vcnECBKjEt/iz8WSUI0jJBxasy7cRuCQcaQayHXydrE=";
+      };
+    })
+  ];
 }
--- config/plug/ui/vim-dadbod-ui.nix
+++ config/plug/ui/vim-dadbod-ui.nix
@@ -1,5 +1,15 @@
+{ pkgs, config, ... }:
 {
-  plugins.vim-dadbod-ui = {
-    enable = true;
-  };
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "vim-dadbod-ui";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "kristijanhusak";
+        repo = "vim-dadbod-ui";
+        rev = "7f89265a84fc003ccfa500fd99b9fea9db2c75c7";
+        hash = "sha256-FjdspE1vwEsmNmeZc9PaGNxS8LXeaX2UainwjaW8F/w=";
+      };
+    })
+  ];
 }

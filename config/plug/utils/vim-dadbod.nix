caf49ac39f13a3d2f2a1ba0a74a859ebe2cd6379
hobo1618 <hobdenw@gmail.com>
2024-12-03 22:04:55 +0100

    build plugin dadbod

--- config/plug/utils/vim-dadbod.nix
+++ config/plug/utils/vim-dadbod.nix
@@ -1,7 +1,15 @@
 { pkgs, config, ... }:
 {
-  plugins.vim-dadbod = {
-    enable = true;
-    # package = pkgs.vimPlugins.vim-dadbod;
-  };
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "vim-dadbod";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "tpope";
+        repo = "vim-dadbod";
+        rev = "fe5a55e92b2dded7c404006147ef97fb073d8b1b";
+        hash = "sha256-wRkd6DRpjDvazJrjbIAndNrnjMIrKOGKQvNfnk+9yeM=";
+      };
+    })
+  ];
 }

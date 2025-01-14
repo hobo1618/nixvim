7c013e9b50ae2dfa64724b85e4e6cec6d8831225
hobo1618 <hobdenw@gmail.com>
2025-01-14 21:45:31 +0100

    update import

--- config/default.nix
+++ config/default.nix
@@ -8,6 +8,7 @@
 
     ./plug/colorscheme/colorscheme.nix
 
+    ./plug/completion/cmp-vimtex.nix
     ./plug/completion/cmp.nix
     ./plug/completion/codecompanion.nix
     ./plug/completion/copilot-cmp.nix

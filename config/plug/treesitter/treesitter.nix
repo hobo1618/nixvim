5c654117409f453759de73fbe15a2b563905ab3c
hobo1618 <hobdenw@gmail.com>
2025-05-09 13:53:30 +0200

    include mdx in treesitter

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -1,23 +1,41 @@
 { pkgs, ... }:
+let
+  mdxGrammar = pkgs.tree-sitter.overrideAttrs (oldAttrs: {
+    pname = "tree-sitter-mdx";
+    version = "unstable-2024-01-01";
+    src = pkgs.fetchFromGitHub {
+      owner = "stone-z";
+      repo = "treesitter-mdx";
+      rev = "6f5a6e3e32b0c89824f157cfa2a972207f20b5e3";
+      sha256 = "sha256-9zZ+tb7v9V9M3h1QlNcs1IMOm2ztJd4Z2NEcXqXAOE4=";
+    };
+  });
+in
 {
-  filetype.extension.liq = "liquidsoap";
+  filetype.extension = {
+    liq = "liquidsoap";
+    mdx = "mdx"; # 👈 ensures .mdx files are detected
+  };
 
   plugins.treesitter = {
     enable = true;
 
     settings = {
-      indent = {
-        enable = true;
-      };
-      highlight = {
-        enable = true;
-      };
+      indent.enable = true;
+      highlight.enable = true;
     };
 
     folding = true;
-    languageRegister.liq = "liquidsoap";
+    languageRegister = {
+      liq = "liquidsoap";
+      mdx = "mdx";
+    };
+
     nixvimInjections = true;
-    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars // {
+      mdx = mdxGrammar;
+    };
   };
 
   extraConfigLua = ''
@@ -28,3 +46,36 @@
     }
   '';
 }
+
+
+
+# { pkgs, ... }:
+# {
+#   filetype.extension.liq = "liquidsoap";
+#
+#   plugins.treesitter = {
+#     enable = true;
+#
+#     settings = {
+#       indent = {
+#         enable = true;
+#       };
+#       highlight = {
+#         enable = true;
+#       };
+#     };
+#
+#     folding = true;
+#     languageRegister.liq = "liquidsoap";
+#     nixvimInjections = true;
+#     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+#   };
+#
+#   extraConfigLua = ''
+#     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+#
+#     parser_config.liquidsoap = {
+#       filetype = "liquidsoap",
+#     }
+#   '';
+# }

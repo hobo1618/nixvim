8bcaf85a8bd37bfbb89177b922286313f60d6da3
hobo1618 <hobdenw@gmail.com>
2025-05-09 14:04:06 +0200

    revert changes

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -1,90 +1,30 @@
 { pkgs, ... }:
-let
-  mdxGrammar = pkgs.tree-sitter.overrideAttrs (oldAttrs: {
-    pname = "tree-sitter-mdx";
-    version = "unstable-2024-01-01";
-    src = pkgs.fetchFromGitHub {
-      owner = "stone-z";
-      repo = "treesitter-mdx";
-      rev = "6f5a6e3e32b0c89824f157cfa2a972207f20b5e3";
-      sha256 = "sha256-9zZ+tb7v9V9M3h1QlNcs1IMOm2ztJd4Z2NEcXqXAOE4=";
-    };
-    passthru.language = "mdx"; # required!
-  });
-in
 {
-  filetype.extension = {
-    liq = "liquidsoap";
-    mdx = "mdx";
-  };
+  filetype.extension.liq = "liquidsoap";
 
   plugins.treesitter = {
     enable = true;
 
     settings = {
-      indent.enable = true;
-      highlight.enable = true;
+      indent = {
+        enable = true;
+      };
+      highlight = {
+        enable = true;
+      };
     };
 
     folding = true;
-    languageRegister = {
-      liq = "liquidsoap";
-      mdx = "mdx";
-    };
-
+    languageRegister.liq = "liquidsoap";
     nixvimInjections = true;
-
-    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      bash
-      css
-      html
-      javascript
-      json
-      lua
-      markdown
-      markdown_inline
-      nix
-      tsx
-      typescript
-      yaml
-      mdxGrammar # 👈 custom grammar injected directly here
-    ];
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
 
   extraConfigLua = ''
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.liquidsoap = { filetype = "liquidsoap" }
+
+    parser_config.liquidsoap = {
+      filetype = "liquidsoap",
+    }
   '';
 }
-
-
-# { pkgs, ... }:
-# {
-#   filetype.extension.liq = "liquidsoap";
-#
-#   plugins.treesitter = {
-#     enable = true;
-#
-#     settings = {
-#       indent = {
-#         enable = true;
-#       };
-#       highlight = {
-#         enable = true;
-#       };
-#     };
-#
-#     folding = true;
-#     languageRegister.liq = "liquidsoap";
-#     nixvimInjections = true;
-#     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
-#   };
-#
-#   extraConfigLua = ''
-#     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-#
-#     parser_config.liquidsoap = {
-#       filetype = "liquidsoap",
-#     }
-#   '';
-# }
